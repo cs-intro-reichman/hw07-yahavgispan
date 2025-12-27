e0b663879752d153847f4611fe46547a6f70c6e3
Yahav Gispan <yahav.gispan@post.runi.ac.il>
2025-12-27 16:12:41 +0200

    7 is done

--- Koch.java
+++ Koch.java
@@ -3,40 +3,52 @@ public class Koch {
 
 	public static void main(String[] args) {
 
-		//// Uncomment the first code block to test the curve function.
-		//// Uncomment the second code block to test the snowflake function.
-		//// Uncomment only one block in each test, and remember to compile
-		//// the class whenever you change the test.
+		/// Uncomment the first code block to test the curve function. Uncomment the
+		/// second code block to test the snowflake function. Uncomment only one block in
+		/// each test, and remember to compile the class whenever you change the test.
 
-        /*
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
-		curve(Integer.parseInt(args[0]),
-			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
-		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+		// curve(Integer.parseInt(args[0]),
+		// Double.parseDouble(args[1]), Double.parseDouble(args[2]),
+		// Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 
-		/*
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+
 	}
 
-	/** Gets n, x1, y1, x2, y2,
-     *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
+	/**
+	 * Gets n, x1, y1, x2, y2,
+	 * and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
+	 */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0)
+			StdDraw.line(x1, y1, x2, y2);
+		else {
+			double oneThirdX = x1 + (1.0 / 3) * (x2 - x1);
+			double twoThirdsX = x1 + (2.0 / 3) * (x2 - x1);
+			double oneThirdY = y1 + (1.0 / 3) * (y2 - y1);
+			double twoThirdsY = y1 + (2.0 / 3) * (y2 - y1);
+			double cornerX = (Math.sqrt(3) / 6) * (oneThirdY - twoThirdsY) + 0.5 * (oneThirdX + twoThirdsX);
+			double cornerY = (Math.sqrt(3) / 6) * (twoThirdsX - oneThirdX) + 0.5 * (oneThirdY + twoThirdsY);
+			curve(n - 1, x1, y1, oneThirdX, oneThirdY);
+			curve(n - 1, oneThirdX, oneThirdY, cornerX, cornerY);
+			curve(n - 1, cornerX, cornerY, twoThirdsX, twoThirdsY);
+			curve(n - 1, twoThirdsX, twoThirdsY, x2, y2);
+
+		}
 	}
 
-    /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
+	/** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		curve(n, 0.1, 0.15, 1.0, 0.15);
+		curve(n, 1.0, 0.15, 0.55, 0.93);
+		curve(n, 0.55, 0.93, 0.1, 0.15);
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -1,18 +1,45 @@
 /** Draws ths Sierpinski Triangle fractal. */
 public class Sierpinski {
-	
+
 	public static void main(String[] args) {
 		sierpinski(Integer.parseInt(args[0]));
 	}
-	
+
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
-	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+	public static void sierpinski(int n) {
+		StdDraw.setCanvasSize(800, 700);
+		StdDraw.setXscale(0, 1);
+		StdDraw.setYscale(0, Math.sqrt(3) / 2);
+		double x1 = 0.0;
+		double y1 = 0.0;
+		double x2 = 0.0;
+		double y2 = 1.0;
+		double x3 = 0.5;
+		double y3 = Math.sqrt(3) / 2;
+		sierpinski(n, x1, x2, x3, y1, y2, y3);
+
 	}
-	
+
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+			double y1, double y2, double y3) {
+		if (n == 0) {
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.line(x2, y2, x3, y3);
+			StdDraw.line(x1, y1, x3, y3);
+			return;
+		}
+
+		double middleX12 = (x1 + x2) / 2;
+		double middleX23 = (x2 + x3) / 2;
+		double middleX13 = (x1 + x3) / 2;
+		double middleY12 = (y1 + y2) / 2;
+		double middleY23 = (y3 + y2) / 2;
+		double middleY13 = (y1 + y3) / 2;
+
+		sierpinski(n - 1, x1, middleX12, middleX13, y1, middleY12, middleY13);
+		sierpinski(n - 1, middleX12, x2, middleX23, middleY12, y2, middleY23);
+		sierpinski(n - 1, middleX13, middleX23, x3, middleY13, middleY23, y3);
+
 	}
 }
